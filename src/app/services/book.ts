876f53e8c2ab73f44c421e8d2cad33724983c42a
UBSITBrettBagleo <20246485@s.ubaguio.edu>
2026-06-30 09:11:44 +0800

    Added BookService CRUD methods

--- src/app/services/book.ts
+++ src/app/services/book.ts
@@ -8,10 +8,25 @@ export class BookService {
 
   private apiUrl = 'http://localhost:3000/books';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getBooks() {
     return this.http.get(this.apiUrl);
   }
 
+  getBook(id: string) {
+    return this.http.get(`${this.apiUrl}/${id}`);
+  }
+
+  addBook(book: any) {
+    return this.http.post(this.apiUrl, book);
+  }
+
+  updateBook(id: string, book: any) {
+    return this.http.put(`${this.apiUrl}/${id}`, book);
+  }
+
+  deleteBook(id: string) {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
 }
