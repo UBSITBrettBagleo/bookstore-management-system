d22694af5e0a7ab581dfb4dde5f8d9cb3f805dde
UBSITBrettBagleo <20246485@s.ubaguio.edu>
2026-06-30 08:44:20 +0800

    Fixed routes/book error

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bookRoutes = require('./routes/books');
+const bookRoutes = require('./routes/book');
 const cors = require('cors');
 require('dotenv').config();
 
