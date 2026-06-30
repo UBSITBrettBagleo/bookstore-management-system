84a60c18c1a06c882c87dddd7fccabf5060b153b
UBSITBrettBagleo <20246485@s.ubaguio.edu>
2026-06-30 10:28:29 +0800

    Implemented add book feature

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { Books } from './pages/books/books';
+import { AddBook } from './pages/add-book/add-book';
 
 export const routes: Routes = [
   {
     path: '',
     component: Books
+  },
+  {
+    path: 'add-book',
+    component: AddBook
   }
 ];
--- src/app/pages/books/books.ts
+++ src/app/pages/books/books.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookService } from '../../services/book';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-books',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './books.html',
   styleUrl: './books.css',
 })
