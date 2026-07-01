2952d18d7e81f3563fc969908a10dc4bf6a4c83a
UBSITBrettBagleo <20246485@s.ubaguio.edu>
2026-07-01 09:45:21 +0800

    Implemented edit book functionality

--- src/app/pages/edit-book/edit-book.ts
+++ src/app/pages/edit-book/edit-book.ts
@@ -1,9 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BookService } from '../../services/book';
 
 @Component({
   selector: 'app-edit-book',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './edit-book.html',
-  styleUrl: './edit-book.css',
+  styleUrl: './edit-book.css'
 })
-export class EditBook {}
+export class EditBook implements OnInit {
+
+  id = '';
+
+  book: any = {
+    title: '',
+    author: '',
+    genre: '',
+    price: 0,
+    stock: 0,
+    coverImage: ''
+  };
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private bookService: BookService
+  ) {}
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get('id')!;
+
+    this.bookService.getBook(this.id).subscribe({
+      next: (data) => {
+        this.book = data;
+      },
+      error: (err) => console.error(err)
+    });
+  }
+
+  updateBook() {
+    this.bookService.updateBook(this.id, this.book).subscribe({
+      next: () => {
+        alert('Book updated successfully!');
+        this.router.navigate(['/']);
+      },
+      error: (err) => console.error(err)
+    });
+  }
+
+}
