0def906b09663199de7b904c51a1ac995939d843
UBSITBrettBagleo <20246485@s.ubaguio.edu>
2026-07-02 09:42:15 +0800

    Updated a working dashboard page

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,9 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BookService } from '../../services/book';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './dashboard.html',
-  styleUrl: './dashboard.css',
+  styleUrl: './dashboard.css'
 })
-export class Dashboard {}
+export class Dashboard implements OnInit {
+
+  totalBooks = 0;
+  totalStock = 0;
+  inventoryValue = 0;
+  totalGenres = 0;
+
+  constructor(private bookService: BookService) {}
+
+  ngOnInit(): void {
+
+    this.bookService.getBooks().subscribe({
+
+      next: (books: any[]) => {
+
+        this.totalBooks = books.length;
+
+        this.totalStock = books.reduce(
+          (sum, book) => sum + Number(book.stock),
+          0
+        );
+
+        this.inventoryValue = books.reduce(
+          (sum, book) => sum + (Number(book.price) * Number(book.stock)),
+          0
+        );
+
+        const genres = new Set(
+          books.map(book => book.genre)
+        );
+
+        this.totalGenres = genres.size;
+
+      },
+
+      error: err => console.log(err)
+
+    });
+
+  }
+
+}
