77b14b74060e618ef0858cee11200d73178408ec
UBSITBrettBagleo <20246485@s.ubaguio.edu>
2026-07-02 09:30:39 +0800

    Added dashboard and other navigation routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,23 @@
 import { Routes } from '@angular/router';
 
 import { Home } from './pages/home/home';
+import { Dashboard } from './pages/dashboard/dashboard';
 import { Books } from './pages/books/books';
 import { AddBook } from './pages/add-book/add-book';
 import { EditBook } from './pages/edit-book/edit-book';
-
+import { About } from './pages/about/about';
+import { Help } from './pages/help/help';
 
 export const routes: Routes = [
 
   {
     path: '',
     component: Home
+  },
+
+  {
+    path: 'dashboard',
+    component: Dashboard
   },
 
   {
@@ -28,7 +35,14 @@ export const routes: Routes = [
     component: EditBook
   },
 
+  {
+    path: 'about',
+    component: About
+  },
 
-
+  {
+    path: 'help',
+    component: Help
+  }
 
 ];
